c938223664413c6e52e02ca144f4e3ab6bd11b58
lirins <747200627@qq.com>
2021-01-04 20:16:13 +0800

    BaseController完成

--- src/main/java/com/lirins/cn/controller/BrandController.java
+++ src/main/java/com/lirins/cn/controller/BrandController.java
@@ -5,15 +5,10 @@ import com.lirins.cn.entity.Brand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @version 0.1
- * @ClassName:BrandController
- * @Description:
- * @date: 2021/1/1
- * @since JDK 1.8
- */
+
 @RestController
 @RequestMapping("brand")
 public class BrandController extends BaseController<Brand,String> {
+
 
 }
