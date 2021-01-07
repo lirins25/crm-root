1800066032122b1b9a4030aa644702f88a65e0bb
lirins25 <747200627@qq.com>
2021-01-07 11:55:12 +0800

    项目搭建基本完成

---
+++ src/main/java/com/lirins/cn/config/MVCConfig.java
@@ -0,0 +1,24 @@
+package com.lirins.cn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @version 0.1
+ * @ClassName:MVCConfig
+ * @Description:
+ * @date: 2021/1/5
+ * @since JDK 1.8
+ */
+@Configuration
+public class MVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
+}
